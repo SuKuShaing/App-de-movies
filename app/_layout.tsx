8cf7ed5db65e647b9e68d97fe0ae1fa9a3771f4a
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-31 13:59:25 -0400

    limpiamos la app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,14 @@
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'react-native';
-import 'react-native-reanimated';
+import React from "react";
+import { Text, View } from "react-native";
+
 import '../global.css';
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const RootLayout = () => {
+	return (
+		<View className="h-full">
+			<Text className="text-4xl font-bold text-blue-500 text-center m-auto">RootLayout</Text>
+		</View>
+	);
+};
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    // <Redirect href="/Aplicacion" />
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+export default RootLayout;
