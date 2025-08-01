0efa6211e182fa5a2d3a1a4bb13ebde079879162
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-01 14:39:55 -0400

    se instala axios, se separan responsabilidades y se crean las interfaces

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
 import React from "react";
 import { Text, View } from "react-native";
-
 import '../global.css';
 
 const RootLayout = () => {
+
+	nowPlayingAction();
+
 	return (
 		<View className="h-full">
 			<Text className="text-4xl font-bold text-blue-500 text-center m-auto">RootLayout</Text>
---
+++ core/actions/movies/now-playing.action.ts
@@ -0,0 +1,14 @@
+import { movieApi } from "@/core/api/movie-api";
+import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-response";
+
+export const nowPlayingAction = async () => {
+    try {
+        const { data } = await movieApi.get<MovieDBMoviesResponse>('/now_playing');
+        console.log(JSON.stringify(data, null, 2));
+
+        return [];
+    } catch (error) {
+        console.log(error);
+        throw 'Can not get now playing movies';
+    }
+}
