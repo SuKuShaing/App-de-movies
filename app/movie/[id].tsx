113ceb4b9cc451c841a74b96a7afa6af98e9e24d
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-07 15:31:19 -0400

    colocando los detalles de la película

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,3 +1,4 @@
+import MovieHeader from "@/presentation/components/movie/MovieHeader";
 import { useMovie } from "@/presentation/hooks/useMovie";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
@@ -9,6 +10,7 @@ const MovieScreen = () => {
 	const { movieQuery } = useMovie(+id); // Para obtener la película, se le envía el + para convertir el id a número
 
 	if (movieQuery.isLoading) {
+		// Si está cargando se muestra el indicador de carga
 		return (
 			<View className="flex-1 items-center justify-center">
 				<Text className="mb-4 text-xl">Espere por favor, Cargando...</Text>
@@ -17,9 +19,23 @@ const MovieScreen = () => {
 		);
 	}
 
+	if (!movieQuery.data) {
+		// Si no hay datos, se muestra el mensaje
+		return (
+			<View className="flex-1 items-center justify-center">
+				<Text className="mb-4 text-xl">Sin datos</Text>
+			</View>
+		);
+	}
+
 	return (
 		<ScrollView>
-			<Text className="m-10 text-2xl font-bold">{movieQuery.data?.title ?? 'Sin título'}</Text>
+			{/* <Text className="m-10 text-2xl font-bold">{movieQuery.data.title}</Text> */}
+			<MovieHeader
+				poster={movieQuery.data.poster}
+				originalTitle={movieQuery.data.originalTitle}
+				title={movieQuery.data.title}
+			/>
 		</ScrollView>
 	);
 };
