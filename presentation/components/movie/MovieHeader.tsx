cbed1117ec851dde330469949871e858790fa409
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-08-07 16:00:24 -0400

    Se aplica un gradiente a la flecha con una librería de Expo

--- presentation/components/movie/MovieHeader.tsx
+++ presentation/components/movie/MovieHeader.tsx
@@ -1,6 +1,13 @@
 import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
-import { Image, Pressable, Text, useWindowDimensions, View } from "react-native";
+import {
+	Image,
+	Pressable,
+	Text,
+	useWindowDimensions,
+	View,
+} from "react-native";
 
 interface Props {
 	poster: string;
@@ -13,27 +20,55 @@ const MovieHeader = ({ poster, originalTitle, title }: Props) => {
 
 	return (
 		<>
-            <View style={{ position: 'absolute', zIndex: 99, elevation: 9, top: 40, left: 10 }}>
-                <Pressable
-                    onPress={() => router.dismiss()}
-                >
-                    <Ionicons name="arrow-back" size={30} color="white" className="shadow" />
-                    {/* shadow solo se ve en ios */}
-                </Pressable>
-            </View>
-            <View
+			{/* Gradiente */}
+			<LinearGradient
+				colors={["rgba(0,0,0,0.4)", "transparent"]}
+				start={{ x: 0, y: 0 }}
+				end={{ x: 0.3, y: 0.3 }}
+				style={{
+					height: screenHeight * 0.4,
+					position: "absolute",
+					zIndex: 1,
+					width: "100%",
+				}}
+			/>
+			{/* Botón de regreso */}
+			<View
+				style={{
+					position: "absolute",
+					zIndex: 99,
+					elevation: 9,
+					top: 40,
+					left: 10,
+				}}
+			>
+				<Pressable onPress={() => router.dismiss()}>
+					<Ionicons
+						name="arrow-back"
+						size={30}
+						color="white"
+						className="shadow"
+					/>
+					{/* shadow solo se ve en ios */}
+				</Pressable>
+			</View>
+			<View
 				className="shadow-xl shadow-black/20"
 				style={{ height: screenHeight * 0.7 }}
 			>
 				<View className="flex-1 rounded-b-[25px] overflow-hidden">
-					<Image source={{ uri: poster }} resizeMode="cover" className="flex-1" />
+					<Image
+						source={{ uri: poster }}
+						resizeMode="cover"
+						className="flex-1"
+					/>
 				</View>
 			</View>
 
-            <View className="px-5 mt-5">
-                <Text className="font-normal">{originalTitle}</Text>
-                <Text className="font-semibold text-2xl">{title}</Text>
-            </View>
+			<View className="px-5 mt-5">
+				<Text className="font-normal">{originalTitle}</Text>
+				<Text className="font-semibold text-2xl">{title}</Text>
+			</View>
 		</>
 	);
 };
